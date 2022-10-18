ccd5fb54536d414dbc902acfe7b6d8d478bcc242
devjean <jeancarloleall@gmail.com>
2022-10-18 00:56:41 -0300

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import "tailwindcss/tailwind.css";
 
 interface Repo {
   name: string;
@@ -8,61 +8,54 @@ interface Repo {
 }
 
 export function App() {
-  const [repos, setRepos] = useState<Repo[]>([])
+  const [repos, setRepos] = useState<Repo[]>([]);
   // const [filteredRepos, setFilteredRepos] = useState<Repo[]>([])
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
-  console.log('Renderizou')
+  console.log("Renderizou");
 
   useEffect(() => {
-    fetch('https://api.github.com/users/Jeanll7/repos')
-      .then(response => response.json())
-      .then(data => setRepos(data))
-  }, [])
+    fetch("https://api.github.com/users/Jeanll7/repos")
+      .then((response) => response.json())
+      .then((data) => setRepos(data));
+  }, []);
 
-  const filteredRepos = search.length > 0
-    ? repos.filter(repo => repo.name.includes(search))
-    : [];
+  const filteredRepos =
+    search.length > 0 ? repos.filter((repo) => repo.name.includes(search)) : [];
 
   // useEffect(() => {
   //   if (search.length) {
   //     setFilteredRepos(repos.filter(repo => repo.name.includes(search)));
   //   }
   // }, [search])
-    
+
   return (
-    <div>
-      <input 
-      name="search"
-      type="text"
-      placeholder="Buscar..."
-      onChange={e => setSearch(e.target.value)}
-      value={search}      
+    <div className="mt-6 ml-6">
+      <h1 className="text-3xl font-bold my-4">RepoGit</h1>
+      <input
+        className="rounded-md p-1 mb-4 text-teal-900 font-semibold tracking-wider"
+        name="search"
+        type="text"
+        placeholder="Buscar..."
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
       />
 
       {search.length > 0 ? (
         <ul>
-          {filteredRepos.map(repo => {
-            return (
-              <li key={repo.name}>
-                  {repo.name}
-              </li>
-            )
+          {filteredRepos.map((repo) => {
+            return <li key={repo.name}>{repo.name}</li>;
           })}
-      </ul>
+        </ul>
       ) : (
         <ul>
-        {repos.map(repo => {
-          return (
-            <li key={repo.name}>
-                {repo.name}
-            </li>
-          )
-        })}
-      </ul>
+          {repos.map((repo) => {
+            return <li key={repo.name}>{repo.name}</li>;
+          })}
+        </ul>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
